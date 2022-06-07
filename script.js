c8ab61286beb0ef26264b708250cf7a12ed05331
slumpthug <gangguchi58@gmail.com>
2022-06-07 14:35:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// Задание 1
+// let arr = [1,2,3,4,5];
+
+// for (i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// Задание 2
+// let arr = [2, 5, 9, 15, 0, 4];
+
+// for (i = 0; i < arr.length; i++) {
+//     if (arr[i] > 3 && arr[i] < 10){
+//         console.log(arr[i]); 
+//     }
+// }
+
+// Задание 3
+// let arr = [10, 20, 30, 50, 235, 3000];
+
+// for (let i = 0; i < arr.length; i++) {
+//     let num = String(arr[i]);
+//     let char = num[0];
+//     if (char == 1 || char == 2 || char == 5) {
+//         console.log(num);
+//     }
+// }
+
+// Задание 4
+// let arr = [1,2,45,"hello", 233, 100, "world", 45];
+
+// console.log(arr.filter(item => typeof item === "number"));
+
+
+// console.log(arr);
+
+// Задание 5
+// let arr = ['Пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+
+// for (i = 0; i < arr.length; i++) {
+//     if (arr[i] == 'сб' || arr[i] == 'вс') {
+//         console.log(arr[i]);
+//     }
+// }
